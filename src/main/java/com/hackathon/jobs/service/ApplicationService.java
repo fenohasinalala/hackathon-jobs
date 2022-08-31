5596ccb81c599bdf583f1461d58ffd7d253a4f18
haritiana adriano <hei.adriano.4@gmail.com>
2022-08-31 11:28:48 +0300

    feat: application post

--- src/main/java/com/hackathon/jobs/service/ApplicationService.java
+++ src/main/java/com/hackathon/jobs/service/ApplicationService.java
@@ -2,18 +2,23 @@ package com.hackathon.jobs.service;
 
 import com.hackathon.jobs.exception.BadRequestException;
 import com.hackathon.jobs.model.Application;
+import com.hackathon.jobs.model.JobOffer;
 import com.hackathon.jobs.repository.ApplicationRepository;
+import com.hackathon.jobs.repository.JobOfferRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ApplicationService {
     private ApplicationRepository applicationRepository;
+    private JobOfferRepository jobOfferRepository;
 
     //GET MAPPING
     public List<Application> getAllApplications(int page, int pageSize, String candidateName, String email, String profile, Double salary){
@@ -35,5 +40,28 @@ public class ApplicationService {
         }else {
             return applicationRepository.findAll(pageable).toList();
         }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //POST MAPPING
+    public Application insertApplication(Application applicationParameter) throws SQLException {
+        Application newApplication = new Application();
+        JobOffer jobOffer;
+        Optional<JobOffer> jobOfferOptional = jobOfferRepository.findById(applicationParameter.getJobOffer().getIdJobOffer());
+
+        if(jobOfferOptional.isPresent()){
+            jobOffer = jobOfferOptional.get();
+        }else {
+            throw new SQLException("job-offer not found");
+        }
+
+        newApplication.setCandidateName(applicationParameter.getCandidateName());
+        newApplication.setDateApplication(applicationParameter.getDateApplication());
+        newApplication.setEmail(applicationParameter.getEmail());
+        newApplication.setProfile(applicationParameter.getProfile());
+        newApplication.setSalary(applicationParameter.getSalary());
+        newApplication.setJobOffer(jobOffer);
+        applicationRepository.save(newApplication);
+        return newApplication;
     }
 }
