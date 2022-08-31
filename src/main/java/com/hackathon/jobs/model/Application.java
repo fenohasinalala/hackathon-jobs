bed33f8bc400e3691c6c19ccbbd20e0da025035c
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2022-08-31 12:04:19 +0300

    fix: fix model and app.properties

--- src/main/java/com/hackathon/jobs/model/Application.java
+++ src/main/java/com/hackathon/jobs/model/Application.java
@@ -1,20 +1,31 @@
 package com.hackathon.jobs.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
+@Table
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Application {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,8 +41,8 @@ public class Application {
     private String email;
 
     private String profile;
-
-    private Double salary;
+    @NotNull(message = "salary is mandatory")
+    private Integer salary;
 
     private LocalDate dateApplication;
 
--- src/main/java/com/hackathon/jobs/model/Domain.java
+++ src/main/java/com/hackathon/jobs/model/Domain.java
@@ -1,6 +1,7 @@
 package com.hackathon.jobs.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,18 +11,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Entity
+@Table
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Domain implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDomain;
 
+    @NotBlank(message = "Domain name is mandatory")
+    @Column(length = 100, unique = true)
     private String name;
 
 }
