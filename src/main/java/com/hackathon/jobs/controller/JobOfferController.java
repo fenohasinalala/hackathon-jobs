754e33ff9118e0953615f440a8eab50ba26d56b5
herizoran <herizo.ran@gmail.com>
2022-08-31 15:25:21 +0300

    Update JobOfferController.java

--- src/main/java/com/hackathon/jobs/controller/JobOfferController.java
+++ src/main/java/com/hackathon/jobs/controller/JobOfferController.java
@@ -4,12 +4,14 @@ import com.hackathon.jobs.model.JobOffer;
 import com.hackathon.jobs.model.Worker;
 import com.hackathon.jobs.service.JobOfferService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 public class JobOfferController {
