fde952f7ecc8c62d242d8cc098a9f4640d287fae
herizoran <herizo.ran@gmail.com>
2022-08-31 11:36:37 +0300

    update job offer

--- src/main/java/com/hackathon/jobs/model/JobOffer.java
+++ src/main/java/com/hackathon/jobs/model/JobOffer.java
@@ -6,11 +6,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Entity
@@ -23,22 +26,33 @@ public class JobOffer implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idJobOffer;
 
+    @NotBlank(message = "Reference is mandatory")
     private String reference;
 
+    @NotBlank(message = "Post is mandatory")
     private String post;
 
+    @NotBlank(message = "Profile is mandatory")
     private String profile;
 
     private String location;
 
     private String description;
 
+    private boolean available;
+
     private String company;
 
-    private String contract;
+    @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Contract is mandatory")
+    private JobOffer.Contract contract;
 
     @ManyToOne
     @JoinColumn(name = "id_domain", nullable = false)
     private Domain domain;
+
+    public enum Contract {
+        CDD,CDI;
+    }
 
 }
