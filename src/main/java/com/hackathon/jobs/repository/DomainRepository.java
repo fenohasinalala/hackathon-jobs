7a8eb536efff739ee4bd0edde41edb74327c76de
herizoran <herizo.ran@gmail.com>
2022-08-31 11:36:57 +0300

    update domain

--- src/main/java/com/hackathon/jobs/repository/DomainRepository.java
+++ src/main/java/com/hackathon/jobs/repository/DomainRepository.java
@@ -1,7 +1,14 @@
 package com.hackathon.jobs.repository;
 
+import com.hackathon.jobs.model.Domain;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface DomainRepository {
+public interface DomainRepository extends JpaRepository<Domain,Long> {
+    List<Domain> findByNameIgnoreCase(String name, Pageable pageable);
+
 }
--- src/main/java/com/hackathon/jobs/service/DomainService.java
+++ src/main/java/com/hackathon/jobs/service/DomainService.java
@@ -1,4 +1,36 @@
 package com.hackathon.jobs.service;
 
+import com.hackathon.jobs.exception.BadRequestException;
+import com.hackathon.jobs.model.Domain;
+import com.hackathon.jobs.repository.DomainRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.Direction.ASC;
+
+@Service
+@AllArgsConstructor
 public class DomainService {
+    private DomainRepository domainRepository;
+
+    //GET MAPPING
+    public List<Domain> getDomains(int page, int pageSize, String name) {
+        if(page<1){
+            throw new BadRequestException("page must be >=1");
+        }
+        if(pageSize>200){
+            throw new BadRequestException("page size too large, must be <=200");
+        }
+        Pageable pageable = PageRequest.of(page - 1,pageSize,
+                Sort.by(ASC,"name"));
+        return domainRepository.findByNameIgnoreCase(name, pageable);
+
+    }
 }
+
+
