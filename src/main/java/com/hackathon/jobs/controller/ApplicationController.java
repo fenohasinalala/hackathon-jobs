24432ad3ba8fb2c5113a79c58e5c6e118aa1fdd5
haritiana adriano <hei.adriano.4@gmail.com>
2022-08-31 12:12:46 +0300

    refactor: application service

--- src/main/java/com/hackathon/jobs/controller/ApplicationController.java
+++ src/main/java/com/hackathon/jobs/controller/ApplicationController.java
@@ -4,9 +4,13 @@ import com.hackathon.jobs.model.Application;
 import com.hackathon.jobs.service.ApplicationService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -24,5 +28,15 @@ public class ApplicationController {
             @RequestParam(name = "salary")Double salary
     ){
         return applicationService.getAllApplications(page, pageSize, candidateName, email, profile, salary);
+    }
+
+    @GetMapping("/applications/{id}")
+    public Application getApplicationById(@PathVariable(name = "id")Long id){
+        return applicationService.getApplicationById(id);
+    }
+
+    @PostMapping("/applications")
+    public Application insertApplication(@RequestBody Application newApplication) throws SQLException {
+        return applicationService.insertApplication(newApplication);
     }
 }
